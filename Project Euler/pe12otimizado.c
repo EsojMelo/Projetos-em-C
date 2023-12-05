ee9b6bdeeb922b4da32495da87109694537ae53d
José <gabriel.melo2002@hotmail.com>
2023-12-05 13:23:01 -0300

    Update

--- Project Euler/pe12otimizado.c
+++ Project Euler/pe12otimizado.c
@@ -1,18 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int countDivisors(long long num) 
-{
-    int count = 0;
-    for (long long i = 1; i * i <= num; i++) 
-    {
-        if (num % i == 0) 
-        {
-            count += (i * i == num) ? 1 : 2;
-        }
-    }
-    return count;
-}
+int countDivisors(long long num);
 
 int main(void) 
 {
@@ -28,4 +17,17 @@ int main(void)
     printf("\nO numero e: %lld", sum);
 
     return 0;
+}
+
+int countDivisors(long long num) 
+{
+    int count = 0;
+    for (long long i = 1; i * i <= num; i++) 
+    {
+        if (num % i == 0) 
+        {
+            count += (i * i == num) ? 1 : 2;
+        }
+    }
+    return count;
 }
